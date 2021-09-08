c1d6eed45e6c9c6ca450a714761ecd1f45eaf643
lukaspet26 <89996794+lukaspet26@users.noreply.github.com>
2021-09-08 06:38:19 +0200

    updated

--- sqlite.go
+++ sqlite.go
@@ -12,6 +12,9 @@ import (
 	"github.com/djthorpe/gopi"
 )
 
+/////////////////////////////////////////////////////////////////////
+// TYPES & INTERFACES
+
 type Type uint
 type Flag uint
 
@@ -20,16 +23,37 @@ type Client interface {
 
 	// Reflect on data structure of a variable to return the rows we expect
 	Reflect(v interface{}) ([]Column, error)
+	PrimaryKey([]Column) (Key, error)
+	//Unique([]Column) ([]Key, error)
+	//Index([]Column) ([]Key, error)
 
-	// Create table
-	CreateTable(table string, columns []Column)
+	// Perform operation and return an error
+	Do(Statement) error
 }
 
 type Column interface {
 	Name() string
+	Identifier() string // Either the name or custom identifier
 	Type() Type
-	Flags() Flag
+	Flag(Flag) bool
+	Value(Flag) string
 }
+
+type Key interface{}
+
+type Statement interface {
+	// CREATE TABLE parameters
+	Schema(string) Statement
+	IfNotExists() Statement
+	Temporary() Statement
+	WithoutRowID() Statement
+
+	// Return SQL string for the statement
+	SQL() string
+}
+
+/////////////////////////////////////////////////////////////////////
+// CONSTANTS
 
 // These are the types we store 'natively' in SQLite
 // in reality, they are converted from the basic types
@@ -43,7 +67,6 @@ const (
 	TYPE_FLOAT
 	TYPE_BLOB
 	TYPE_TIME
-	TYPE_DURATION
 	TYPE_MAX
 )
 
@@ -52,9 +75,12 @@ const (
 const (
 	FLAG_NONE     Flag = 0
 	FLAG_NOT_NULL Flag = (1 << iota)
-	FLAG_UNIQUE
 	FLAG_PRIMARY_KEY
-	FLAG_MAX
+	FLAG_UNIQUE_KEY
+	FLAG_INDEX_KEY
+	FLAG_NAME
+	FLAG_TYPE
+	FLAG_MAX = FLAG_TYPE
 )
 
 /////////////////////////////////////////////////////////////////////
@@ -78,9 +104,26 @@ func (t Type) String() string {
 		return "TYPE_BLOB"
 	case TYPE_TIME:
 		return "TYPE_TIME"
-	case TYPE_DURATION:
-		return "TYPE_DURATION"
 	default:
 		return "[?? Invalid Type value]"
+	}
+}
+
+func (f Flag) String() string {
+	switch f {
+	case FLAG_NOT_NULL:
+		return "FLAG_NOT_NULL"
+	case FLAG_PRIMARY_KEY:
+		return "FLAG_PRIMARY_KEY"
+	case FLAG_UNIQUE_KEY:
+		return "FLAG_UNIQUE_KEY"
+	case FLAG_INDEX_KEY:
+		return "FLAG_INDEX_KEY"
+	case FLAG_NAME:
+		return "FLAG_NAME"
+	case FLAG_TYPE:
+		return "FLAG_TYPE"
+	default:
+		return "[?? Invalid Flag value]"
 	}
 }
--- v3/sqlite.go
+++ v3/sqlite.go
@@ -10,10 +10,7 @@ package v3
 
 import (
 	"database/sql/driver"
-	"errors"
 	"fmt"
-	"reflect"
-	"strings"
 
 	gopi "github.com/djthorpe/gopi"
 	sqlite "github.com/djthorpe/sqlite"
@@ -34,10 +31,17 @@ type client struct {
 }
 
 type column struct {
-	n string
-	t sqlite.Type
-	f sqlite.Flag
+	n string                 // column name
+	t sqlite.Type            // sql type
+	f map[sqlite.Flag]string // flag-value tag pairs
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+
+const (
+	_V3_TAG = "sql"
+)
 
 ////////////////////////////////////////////////////////////////////////////////
 // OPEN AND CLOSE
@@ -50,8 +54,10 @@ func (config Client) Open(log gopi.Logger) (gopi.Driver, error) {
 	this.log = log
 
 	d := &sqlite_driver.SQLiteDriver{}
-	if _, err := d.Open(config.DSN); err != nil {
+	if conn, err := d.Open(config.DSN); err != nil {
 		return nil, err
+	} else {
+		this.conn = conn
 	}
 
 	// Return success
@@ -61,123 +67,20 @@ func (config Client) Open(log gopi.Logger) (gopi.Driver, error) {
 // Close releases any resources associated with the client connection
 func (this *client) Close() error {
 	this.log.Debug("<sqlite.v3.Client>Close{ }")
-	return nil
+	return this.conn.Close()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // INTERFACE
 
-// Return description of rows
-func (this *client) Reflect(v interface{}) ([]sqlite.Column, error) {
-	// Dereference the pointer
-	v2 := reflect.ValueOf(v)
-	for v2.Kind() == reflect.Ptr {
-		v2 = v2.Elem()
-	}
-	// If not a stuct then return
-	if v2.Kind() != reflect.Struct {
-		return nil, errors.New("Called Reflect on a non-struct")
-	}
-	// Enumerate struct fields
-	columns := make([]sqlite.Column, v2.NumField())
-	t2 := v2.Type()
-	for i := 0; i < t2.NumField(); i++ {
-		fieldType := t2.Field(i)
-		column, err := columnFor(fieldType)
-		if err != nil {
-			return nil, err
-		}
-		if fieldTag, ok := fieldType.Tag.Lookup("sql"); ok {
-			if tagName, tagType, tagFlags, err := parseTag(fieldTag); err != nil {
-				return nil, err
-			} else {
-				// Override column information from tag
-				if tagName != "" {
-					column.n = tagName
-				}
-				if tagType != sqlite.TYPE_NONE {
-					column.t = tagType
-				}
-				column.f |= tagFlags
-			}
-		}
-		columns[i] = column
-	}
-	return columns, nil
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE METHODS
-
-func columnFor(f reflect.StructField) (*column, error) {
-	fmt.Printf("name %v => %v type %v\n", f.Name, f.PkgPath, typeFor(f.Type))
-	return &column{
-		n: f.Name,
-		t: typeFor(f.Type),
-	}, nil
-}
-
-func typeFor(t reflect.Type) sqlite.Type {
-	switch {
-	case t.Kind() == reflect.String:
-		return sqlite.TYPE_TEXT
-	case t.Kind() == reflect.Bool:
-		return sqlite.TYPE_BOOL
-	case t.Kind() == reflect.Uint || t.Kind() == reflect.Uint8 || t.Kind() == reflect.Uint16 || t.Kind() == reflect.Uint32 || t.Kind() == reflect.Uint64:
-		return sqlite.TYPE_UINT
-	case t.Kind() == reflect.Int || t.Kind() == reflect.Int8 || t.Kind() == reflect.Int16 || t.Kind() == reflect.Int32 || t.Kind() == reflect.Int64:
-		return sqlite.TYPE_INT
-	case t.Kind() == reflect.Float32 || t.Kind() == reflect.Float64:
-		return sqlite.TYPE_FLOAT
-	default:
-		return sqlite.TYPE_NONE
-	}
-}
-
-func parseTag(tag string) (string, sqlite.Type, sqlite.Flag, error) {
-	f := sqlite.FLAG_NONE
-	n := ""
-	if fields := strings.Split(tag, ";"); len(fields) != 0 {
-		for _, field := range fields {
-			if kv := strings.SplitN(field, ":", 2); len(kv) > 0 && kv[0] != "" {
-				switch kv[0] {
-				case "name":
-					if len(kv) != 2 {
-						return "", 0, 0, fmt.Errorf("Parse error in tag '%v': Invalid name", tag)
-					} else {
-						n = kv[1]
-					}
-					break
-				case "type":
-					break
-				default:
-					if len(kv) != 1 {
-						return "", 0, 0, fmt.Errorf("Parse error in tag '%v': Invalid flag '%v'", tag, kv[0])
-					} else if flag, err := parseFlag(kv[0]); err != nil {
-						return "", 0, 0, fmt.Errorf("Parse error in tag '%v': %v", tag, err)
-					} else {
-						f |= flag
-					}
-				}
-				fmt.Printf("KV %v %v\n", len(kv), kv)
-			}
-		}
-	}
-	return n, 0, f, nil
-}
-
-func parseFlag(flag string) (sqlite.Flag, error) {
-	switch flag {
-	case "index":
-		return sqlite.FLAG_INDEX, nil
-	case "not null", "notnull", "not_null":
-		return sqlite.FLAG_NOT_NULL, nil
-	case "unique_key", "unique key", "unique":
-		return sqlite.FLAG_UNIQUE, nil
-	case "primary_key", "primary key", "primarykey", "primary":
-		return sqlite.FLAG_PRIMARY_KEY, nil
-	default:
-		return sqlite.FLAG_NONE, fmt.Errorf("Unknown flag '%v'", flag)
+func (this *client) Do(statement sqlite.Statement) error {
+	this.log.Debug("<sqlite.v3.Client>Do{ %v }", statement.SQL())
+	if s, err := this.conn.Prepare(statement.SQL()); err != nil {
+		return err
+	} else if _, err := s.Exec([]driver.Value{}); err != nil {
+		return err
+	} else {
+		return nil
 	}
 }
 
@@ -188,14 +91,41 @@ func (this *column) Name() string {
 	return this.n
 }
 
+func (this *column) Identifier() string {
+	if this.Flag(sqlite.FLAG_NAME) == false {
+		return this.n
+	} else if value := this.Value(sqlite.FLAG_NAME); value != "" {
+		return value
+	} else {
+		return this.n
+	}
+}
+
 func (this *column) Type() sqlite.Type {
 	return this.t
 }
 
-func (this *column) Flags() sqlite.Flag {
-	return this.f
+func (this *column) Flag(flag sqlite.Flag) bool {
+	_, ok := this.f[flag]
+	return ok
+}
+
+func (this *column) Value(flag sqlite.Flag) string {
+	if v, ok := this.f[flag]; ok {
+		return v
+	} else {
+		return ""
+	}
 }
 
 func (this *column) String() string {
-	return fmt.Sprintf("<sqlite.v3.Column>{ name=%v type=%v flags=%v }", this.n, this.t, this.f)
+	flags := ""
+	for k, v := range this.f {
+		if v == "" {
+			flags += fmt.Sprint(k) + " "
+		} else {
+			flags += fmt.Sprint(k) + "='" + v + "' "
+		}
+	}
+	return fmt.Sprintf("<sqlite.v3.Column>{ name=%v type=%v %v}", this.n, this.t, flags)
 }
